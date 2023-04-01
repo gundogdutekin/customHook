403590bbb511814edab4fc1cca877374c866792f
tekin <gundogdutekin@gmail.com>
2023-04-02 01:59:51 +0300

    updates to buttons

--- src/TitleOne.jsx
+++ src/TitleOne.jsx
@@ -3,10 +3,18 @@ import useTitleCountDisplay from './useTitleCountDisplay'
 
 export default function TitleOne() {
     const [count, setCount] = useState(0)
+    const increment = () => setCount(a=>a+1)
+    const decrement = () => setCount(a=>a-1)
+    const reset = () => setCount(0)
 
     useTitleCountDisplay(count)//useTitleCountDisplay is a custom hook
         
   return (
-    <div><button onClick={()=>{setCount(count+1)}}>Sayı = {count}</button></div>
+    <div>
+      <h1>{count}</h1>
+      <button className='btn btn-green' onClick={increment}>Artır</button>
+      <button className='btn btn-red' onClick={decrement}>Azalt</button>
+      <button className='btn btn-default' onClick={reset}>Sıfırla</button>
+      </div>
   )
 }
